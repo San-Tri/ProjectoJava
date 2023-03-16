5812d86b32baf6e12a8b1abba26e32d0382f80f4
San-Tri <1473024@senati.pe>
2023-03-16 15:40:53 -0500

    java.4.

--- projectojava/ProjectoJava.java
+++ projectojava/ProjectoJava.java
@@ -8,16 +8,16 @@ public class ProjectoJava {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.println("Introduse tu nombre");
-        String nombre;
+        String nombre = sc.nextLine();
         Scanner teclado = new Scanner(System.in);
         nombre = teclado.nextLine();
         System.out.println("¡Hola " + nombre + "!");
         int número = sc.nextLine();
         if(número<= 10) {
             System.out.println("El número es menor que 10");
-    }else{
-        System.out.println("El número es menor que 10");
-    }
+}else{
+            System.out.println("El número es menor que 10");
         }
+    }
     
 }
