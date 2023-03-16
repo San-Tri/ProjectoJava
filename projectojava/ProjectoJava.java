8a3906f2562b3ffb23bcf5e6bff022af84f9201e
San-Tri <1473024@senati.pe>
2023-03-16 11:18:00 -0500

    java.2.

--- projectojava/ProjectoJava.java
+++ projectojava/ProjectoJava.java
@@ -1,20 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package projectojava;
-
-/**
- *
- * @author Alumno
- */
+import java.util.Scanner;
 public class ProjectoJava {
 
-    /**
-     * @param args the command line arguments
+    /*
+     Elaborar un algoritmo que permita el ingreso de un número si es mayor o menor que "10"
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Introduse tu nombre");
+        String nombre;
+        Scanner teclado = new Scanner(System.in);
+        nombre = teclado.nextLine();
+        System.out.println("¡Hola " + nombre + "!");
+        System.out.print("Introduzca primer número: ");
+        int x, y;
+        x = teclado.nextInt();
+        System.out.print("Introduzca segundo número: ");
+        y = teclado.nextInt();
+        System.out.println(x + "*" + y + "=" + (x*y));
     }
     
 }
