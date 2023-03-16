46d30124c642ca29af750e6dfc94441b63e245d5
San-Tri <1473024@senati.pe>
2023-03-16 15:31:26 -0500

    java.3.

--- projectojava/ProjectoJava.java
+++ projectojava/ProjectoJava.java
@@ -6,17 +6,18 @@ public class ProjectoJava {
      Elaborar un algoritmo que permita el ingreso de un número si es mayor o menor que "10"
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
         System.out.println("Introduse tu nombre");
         String nombre;
         Scanner teclado = new Scanner(System.in);
         nombre = teclado.nextLine();
         System.out.println("¡Hola " + nombre + "!");
-        System.out.print("Introduzca primer número: ");
-        int x, y;
-        x = teclado.nextInt();
-        System.out.print("Introduzca segundo número: ");
-        y = teclado.nextInt();
-        System.out.println(x + "*" + y + "=" + (x*y));
+        int número = sc.nextLine();
+        if(número<= 10) {
+            System.out.println("El número es menor que 10");
+    }else{
+        System.out.println("El número es menor que 10");
     }
+        }
     
 }
