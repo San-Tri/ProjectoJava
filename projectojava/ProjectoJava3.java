a23ce3a60299d42d2cef72da8be0eaa56687f060
San-Tri <1473024@senati.pe>
2023-03-17 16:05:55 -0500

    sumaJava.3

--- projectojava/ProjectoJava3.java
+++ projectojava/ProjectoJava3.java
@@ -1,11 +1,13 @@
+package ProjectoJava3;
 //importamos la clase Scanner
 import java.util.Scanner;
 public class ProjectoJava3 {
+    public static void main (String[] args) {
     //llamamos a la funcion Scanner
     Scanner sc = new Scanner(System.in);
     //Pedir el nombre al usuario
     System.out.println("Introduse tu nombre");
     String nombre = name.nextLine();
     System.out.println("¡Hola " + nombre);
-    Scanner escaner = new Scanner(System.in);
+    }
 }
