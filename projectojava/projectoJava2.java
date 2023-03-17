da840698efacd1120e43dd1875573227e8542df1
San-Tri <1473024@senati.pe>
2023-03-17 10:44:51 -0500

    Exercise-if

---
+++ projectojava/projectoJava2.java
@@ -0,0 +1,15 @@
+package projectoJava2;
+import java.util.Scanner;
+public class projectoJava2 {
+    /*
+    Desarrollar un algoitmo que indique si el número ingresado es poditivo o negativo
+    */
+    public static void main(String[] args) {
+        //llamar a la funcion escaner
+        Scanner name = new Scanner (System.in);
+        //pedir al usuario su nombre
+        System.out.println("Introduse tu nombre");
+        }
+    }
+    
+}
