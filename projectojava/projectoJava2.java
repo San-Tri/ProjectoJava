9bf0273d20aae49d929cd367f945f9fd80be8c41
San-Tri <1473024@senati.pe>
2023-03-17 11:33:31 -0500

    Exercise-if.2

--- projectojava/projectoJava2.java
+++ projectojava/projectoJava2.java
@@ -2,14 +2,22 @@ package projectoJava2;
 import java.util.Scanner;
 public class projectoJava2 {
     /*
-    Desarrollar un algoitmo que indique si el número ingresado es poditivo o negativo
+    Desarrollar un algoritmo que indique si el número ingresado es poditivo o negativo
     */
     public static void main(String[] args) {
         //llamar a la funcion escaner
         Scanner name = new Scanner (System.in);
         //pedir al usuario su nombre
         System.out.println("Introduse tu nombre");
+        String nombre = name.nextLine();
+        System.out.println("¡Hola " + nombre);
+        Scanner escaner = new Scanner(System.in);
+		// pedir el número al usuario
+		System.out.println("Escribe un número:");
+        double numero = escaner.nextDouble();
+		
+
+
+
         }
     }
-    
-}
