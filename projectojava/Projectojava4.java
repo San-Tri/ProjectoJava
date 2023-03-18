596d7e677978176521568fc37d59414617775928
San-Tri <1473024@senati.pe>
2023-03-18 10:54:14 -0500

    Npares.2

--- projectojava/Projectojava4.java
+++ projectojava/Projectojava4.java
@@ -1,7 +1,13 @@
 package Pojectojava4;
+
+import java.util.ArrayList;
+
 public class JavaApplication19 {
 public static void main(String[] args) {
         //Desarollar un algoritmo que muestre la suma de los números pares.
-        
+        //primero definimos un areglo:
+        ArrayList<Double> Par = new ArrayList<>();
+        ArrayList<Double> ImPar = new ArrayList<>();
+
     }
 }
