a8a0b24324ad9cbeea59c5b5cb7046f420a2118a
San-Tri <1473024@senati.pe>
2023-03-18 10:49:15 -0500

    Npares.

---
+++ projectojava/Projectojava4.java
@@ -0,0 +1,7 @@
+package Pojectojava4;
+public class JavaApplication19 {
+public static void main(String[] args) {
+        //Desarollar un algoritmo que muestre la suma de los números pares.
+        
+    }
+}
