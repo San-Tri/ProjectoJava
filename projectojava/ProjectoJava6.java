e5bc17894ae23933bbddbac969d7c6ee7a77b5e1
San-Tri <1473024@senati.pe>
2023-03-23 10:43:36 -0500

    sumaWhile

---
+++ projectojava/ProjectoJava6.java
@@ -0,0 +1,20 @@
+package ProjectoJava6;
+//importar escanner
+import java.util.Scanner;
+/**
+ *
+ * @author Alumno
+ */
+public class JavaApplication27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here}
+        Scanner dato = new Scanner(System.in);
+        System.out.println("Ingresa el número: ");
+        int n = dato.nextInt();
+    }
+    
+}
