c3202a19b4258d430184629a3d8d50fa0ff37596
San-Tri <1473024@senati.pe>
2023-03-23 11:31:53 -0500

    sumaWhile.2

--- projectojava/ProjectoJava6.java
+++ projectojava/ProjectoJava6.java
@@ -1,20 +1,14 @@
 package ProjectoJava6;
 //importar escanner
 import java.util.Scanner;
-/**
- *
- * @author Alumno
- */
 public class JavaApplication27 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here}
         Scanner dato = new Scanner(System.in);
+        int sumaPares = 0;
+        int numero = 0;
+        //pedimos el nombre
         System.out.println("Ingresa el número: ");
-        int n = dato.nextInt();
+        numero = dato.nextInt();
     }
     
 }
