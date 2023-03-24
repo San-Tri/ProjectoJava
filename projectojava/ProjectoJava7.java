b333845bb4beacbc940aa0a126b2a68c0aefb2c7
San-Tri <1473024@senati.pe>
2023-03-24 10:54:12 -0500

    JAVA

---
+++ projectojava/ProjectoJava7.java
@@ -0,0 +1,7 @@
+package ProjectoJava7;
+public class ProjectoJava7 {
+    public static void main(String[] args) {
+        //crear la palabra java con java
+        System.out.println("    j     a   v       v    a");
+    }
+}
