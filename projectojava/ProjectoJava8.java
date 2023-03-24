5b1e115afed91f1daafcb35b2f015de8c2451e1d
San-Tri <1473024@senati.pe>
2023-03-24 11:14:49 -0500

    ==

---
+++ projectojava/ProjectoJava8.java
@@ -0,0 +1,20 @@
+package ProjectoJava8;
+
+public class ProjectoJava8 {
+    public static void main(String[] args) {
+    int vble1 = 10;
+        int vble2 = 20;
+
+    if (vble1 != vble2)
+        System.out.println(" 10 != 20");
+
+    if (vble1 < vble2)
+        System.out.println(" 10 < 20");
+
+    if (vble1 >= vble2)
+        System.out.println(" 10 >= 20");
+
+    if (vble1 <= vble2)
+        System.out.println(" 10 <= 20");
+    }
+}
