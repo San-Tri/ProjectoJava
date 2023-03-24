fcfc966b0ba945cb8167a8b20ae8ac7dfbc16c80
San-Tri <1473024@senati.pe>
2023-03-24 10:56:50 -0500

    JAVA.2

--- projectojava/ProjectoJava7.java
+++ projectojava/ProjectoJava7.java
@@ -3,5 +3,8 @@ public class ProjectoJava7 {
     public static void main(String[] args) {
         //crear la palabra java con java
         System.out.println("    j     a   v       v    a");
+        System.out.println("    j    a a   v     v    a  a");
+        System.out.println(" j  j   aaaaa   v   v    aaaaaa");
+        System.out.println("  jj   a     a    v     a      a");
     }
 }
