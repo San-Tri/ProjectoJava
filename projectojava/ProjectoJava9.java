e66e883f319449c638d473e822bfc7b1c44a11c6
San-Tri <1473024@senati.pe>
2023-03-25 08:21:54 -0500

    welcome.3

--- projectojava/ProjectoJava9.java
+++ projectojava/ProjectoJava9.java
@@ -3,8 +3,8 @@ public class ProjectoJava9 {
     System.out.println("        Hello, Have a good day");
     System.out.println("                 oh! Thanks you");
     System.out.println("                                   ");
-    System.out.println("Morning, Morning, Morning");
-    System.out.println("Morning, Morning, Morning");
-    System.out.println("Morning, Morning, Morning");
+    System.out.println("                 You are great");
+    System.out.println("        this is my pleasureg");
+    System.out.println("No,... You are welcome");
 
 }
