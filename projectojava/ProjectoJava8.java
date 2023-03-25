f4bcac15a41d440b1b644c141548eedb94e68ca4
San-Tri <1473024@senati.pe>
2023-03-25 08:50:55 -0500

    ==.5

--- projectojava/ProjectoJava8.java
+++ projectojava/ProjectoJava8.java
@@ -4,20 +4,23 @@ public class ProjectoJava8 {
     public static void main(String[] args) {
         //llamar a la fusion escaner
         Scanner vble = new Scanner (System.in);
-        
-        System.out.println("ingrese el numero");
-        System.out.println("ingrese el segundo numero");
+        //pedir el ingreso de los numeros
+        System.out.println("ingrese el numero: ");
+        int n1 = sc.nextInt ();
+        System.out.println("ingrese el segundo numero: ");
+        int n2 = sc.nextInt ();
         //Usamos operadores de igualdad
-        int vble1 = 10;
-        int vble2 = 20;
+        int vble1 = n1;
+        int vble2 = n2;
         //Validamos la relacion de los valores
-    if (vble1 != vble2)
-        System.out.println(" 10 != 20"+" es diferente");
-
-    if (vble1 < vble2)
-        System.out.println(" 10 < 20"+" es menor");
-
-    if (vble1 <= vble2)
-        System.out.println(" 10 <= 20"+"es menor o igual");
+    if (vble1 != vble2) {
+        System.out.println( n1 + "!=" + n2 +" es diferente");
+    }
+    if (vble1 < vble2) {
+        System.out.println(n1 + "<" + n2 + " es menor");
+    }
+    if (vble1 <= vble2) {
+        System.out.println(n1 + "<=" + n2 + "es menor o igual");
+    }
     }
 }
