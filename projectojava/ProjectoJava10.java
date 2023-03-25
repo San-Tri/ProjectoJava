74688afa28d064e99f08e2c4e7bc1aacf8fa8ee5
San-Tri <1473024@senati.pe>
2023-03-25 10:18:07 -0500

    999.2

--- projectojava/ProjectoJava10.java
+++ projectojava/ProjectoJava10.java
@@ -1,6 +1,12 @@
 package ProjectoJava10;
+import java.util.Scanner;
 public class ProjectoJava10 {
     public static void main(String[] args) {
+        System.out.println("ingrese el primer numero: ");
+        int n1 = n.nextInt ();
+        System.out.println("ingrese el segundo numero: ");
+        int n2 = n.nextInt ();
+
         for (int i = 1; i <= 999; i++){
             if (i % 2 !=0){
                 System.out.println(i);
